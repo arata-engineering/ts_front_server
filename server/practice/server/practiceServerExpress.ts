59e60d0f45bdfe3b1fed665ea5c3b3ba22e368d2
toshimaArata <wada.a015@gmail.com>
2024-07-21 02:35:56 +0900

    redis

--- server/practice/server/practiceServerExpress.ts
+++ server/practice/server/practiceServerExpress.ts
@@ -31,6 +31,39 @@ app.get("/next/", (req, res, next) => {
     res.send(`<html><h1>next page</h1></html>`);
 });
 
+/** Redisデータ取得後にJSON返却 */
+app.get("/user/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const valueOfRedis: string | null = await redis.get(userId);
+    if (valueOfRedis === null) {
+        next(new Error("redis get Error"));
+        return;
+    }
+    const json = JSON.parse(valueOfRedis);
+    res.status(200).json(json);
+});
+app.get("/users/", async (req, res, next) => {
+    try {
+        const keyarr: string[] = await redis.keys("User*");
+        console.log(`keys : ${keyarr}`);
+
+        const stream = redis.scanStream({
+            match: "User*"
+        });
+        const users: string[] = [];
+        for await (const resultKeys of stream) {
+            for (const key of resultKeys) {
+                const value = await redis.get(key);
+                const json: any = JSON.parse(value as string);
+                users.push(json);
+            }
+        }
+        res.status(200).send(users);
+    } catch(e) {
+        next(e);
+    }
+});
+
 /** 包括的エラーハンドリング */
 /**
  * 下記の条件の時に4つの引数を設定した#useの包括的エラーハンドリングへ遷移します。
@@ -43,9 +76,13 @@ app.get("/error/", (req, res, next) => {
     throw new Error("This is error");
 });
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log("===包括的エラーハンドリング===");
     res.status(500).send(`<html><h1>error page : ${err}</h1></html>`);
 });
 
+/**
+ * サーバ起動とRedis通信
+ */
 const redis: Redis = new Redis({
     port: 6379,
     host: "localhost",
@@ -53,8 +90,9 @@ const redis: Redis = new Redis({
 });
 const redisInit = async () => {
     await Promise.all([
-        redis.set("User1", JSON.stringify({id: 1, name: "alpha"})),
-        redis.set("User2", JSON.stringify({id: 1, name: "bravo"}))
+        redis.set("User1", JSON.stringify({ id: 1, name: "alpha" })),
+        redis.set("User2", JSON.stringify({ id: 2, name: "bravo" })),
+        redis.set("User3", JSON.stringify({ id: 3, name: "trip" }))
     ]);
 }
 redis.once("ready", async () => {
@@ -65,8 +103,8 @@ redis.once("ready", async () => {
         console.log(e);
     }
 });
-
 redis.on("error", (err) => {
-   console.log(err);
-   process.exit(1); 
+    console.log("===redis error===")
+    console.log(err);
+    process.exit(1);
 });
